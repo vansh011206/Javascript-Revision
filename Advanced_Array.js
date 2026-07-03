b5b1d92f5f48a88c28dd062947cb4092598d8ccf
vansh011206 <vanshajs111@gmail.com>
2026-07-03 21:32:00 +0530

    new

---
+++ Advanced_Array.js
@@ -0,0 +1,65 @@
+// 1
+[20,30,40]
+
+// 2
+[20]
+
+// 3
+1
+
+// 4
+false
+
+// 5
+[1,2,3,4]
+
+// 6
+A-B-C
+
+// 7
+[3,2,1]
+
+// 8
+[1,2,5,8]
+
+// 9
+let fruits=["Apple","Banana","Mango"];
+console.log(fruits.includes("Mango"))
+
+// 10
+let arr=[10,20,30,40];
+console.log(arr.slice(1,2));
+console.log(arr.slice(2,3));
+
+// 11
+[3]
+
+// 12
+-1
+
+// 13
+let a=[10,20];
+
+let b=[30,40];
+console.log(a.concat(b))
+
+// 14
+slice() -  
+// - iska syntax array.slice(starting index ,ending index (exluded))
+// - ye new array banata h copied , exisiting ko change ni karta
+splice()
+// - iska syntax array.splice(starting index ,count of element which we want to exclude , any quantity which we want to insert on that index)
+// - ye existing array ko change karta h 
+
+// 15
+// indexOf(x) - yeh dekhta h ki array x hai ya ni , pehla index dhundhta h agar mila toh return karta h kya index pe hai vo value and nhi toh -reurtn karta h -1 if nhi milta toh uss array m x
+// includes() -  yeh bhi check karta h , pr bas ye yeh batata ki araay m hai ya nhi true ya false ki form m 
+
+// bonus 
+[10,20]
+
+
+
+
+
+
--- try.js
+++ try.js
@@ -1,62 +1,99 @@
-for(let i=1; i<=3;i++){
+// for(let i=1; i<=3;i++){
 
-    for(let j=i;j<=3;j++){
-        console.log(j);
-    }
-}
+//     for(let j=i;j<=3;j++){
+//         console.log(j);
+//     }
+// }
 
-for(let i = 1; i <= 3; i++) {
+// for(let i = 1; i <= 3; i++) {
 
-    let row = "";
+//     let row = "";
 
-    for(let j = 1; j <= 3; j++) {
-        row += j;
-    }
+//     for(let j = 1; j <= 3; j++) {
+//         row += j;
+//     }
 
-    console.log(row);
+//     console.log(row);
 
-}
-
-
-1
-12
-123
-1234
-12345
-
-for(let i=1; i<=5;i++){
-      row = "";
-      for(let j=1; j<=i; j++){
-          row += j;
-      }
-      console.log(row);
-}
-
-54321
-5432
-543
-54
-5
-for(let i=5; i>=1;i--){
-      row = "";
-      for(let j=5; j>=i; j--){
-          row += j
-      }
-      console.log(row);
-}
+// }
 
 
-const positive = (num) => {
-    if(num < 0){
-        console.log("Negative")
-    }
-    else if(num === 0 ){
-        console.log("Zero")
-    }
-    else{
-        console.log("Positive")
-    }
-}
+// 1
+// 12
+// 123
+// 1234
+// 12345
 
-positive(10);
-positive(-5);
+// for(let i=1; i<=5;i++){
+//       row = "";
+//       for(let j=1; j<=i; j++){
+//           row += j;
+//       }
+//       console.log(row);
+// }
+
+// 54321
+// 5432
+// 543
+// 54
+// 5
+// for(let i=5; i>=1;i--){
+//       row = "";
+//       for(let j=5; j>=i; j--){
+//           row += j
+//       }
+//       console.log(row);
+// }
+
+
+// const positive = (num) => {
+//     if(num < 0){
+//         console.log("Negative")
+//     }
+//     else if(num === 0 ){
+//         console.log("Zero")
+//     }
+//     else{
+//         console.log("Positive")
+//     }
+// }
+
+// positive(10);
+// positive(-5);
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// splice(startIndex, deleteCount, ...newItems)
+
+// // Remove karna
+// arr.splice(1, 2); 
+// console.log(arr); // [1, 4, 5]
+
+// Replace karna
+// let arr2 = [1, 2, 3];
+// arr2.splice(1, 1, "X"); // index 1 pe 1 element remove karo, "X" daalo
+// console.log(arr2); // [1, "X", 3]
+
+// // Insert karna (delete count 0)
+// let arr3 = [1, 2, 3];
+// arr3.splice(1, 0, "A", "B");
+// console.log(arr3); // [1, "A", "B", 2, 3]
+
+
+// console.log(arr)
+
+
+let arr = ["Hello", "World", "JS"];
+let arr2 = [1,2,3,4,5]
+// console.log(arr.join()); // "Hello,World,JS" (default comma)
+// console.log(arr.join(" ")); // "Hello World JS"
+// console.log(arr.join("-")); // "Hello-World-JS"
+// console.log(arr.join(""))
+
+
+let combine=  arr.concat(arr2)
+console.log(combine.length)
+console.log(combine)
+
+
